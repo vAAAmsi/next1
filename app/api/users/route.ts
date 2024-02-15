1e6d3a2b575fae050421e1115c628c6fb631394d
vamsi <mamillapallivamsi11@gmail.com>
2024-02-15 22:06:05 +0530

    build issues

--- app/api/users/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-const user = { name: 'vamsi' };
-
-export async function GET() {
-   
-}
--- app/components/DataList.tsx
+++ app/components/DataList.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import React from 'react'
 import { usertype } from '../types'
 import Link from 'next/link'
